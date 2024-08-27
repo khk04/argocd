2793f1f68778b4cf380018106ec39b86ce82718b
김현기 <khkraining@juxtagene.com>
2024-08-27 10:39:24 +0900

    chore: Add argocd-server deployment status check

--- argocd/cli/deploy-apply-argocd.sh
+++ argocd/cli/deploy-apply-argocd.sh
@@ -25,3 +25,13 @@ fi
 
 kubectl --kubeconfig "$KUBE_CONFIG" ${DEPLOY_TYPE} --validate=false -n ${NAME_SPACE} -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.11.7/manifests/install.yaml
 kubectl --kubeconfig "$KUBE_CONFIG" ${DEPLOY_TYPE} -n ${NAME_SPACE} -f ../manifests/ingress.yaml
+
+echo "argocd-server deployment 상태 확인 중..."
+while true; do
+    SERVICE_STATUS=$(kubectl --kubeconfig "$KUBE_CONFIG" get deployment argocd-server -n argocd -o jsonpath='{.status.conditions[?(@.type=="Available")].status}')
+    if [ "$SERVICE_STATUS" = "True" ]; then
+        break
+    fi
+    sleep 5
+done
+echo "argocd-server deployment 상태: $SERVICE_STATUS"
